e39bf612034fea038ce1070bc8738ca2d515b8ed
AliTinai <ali-009@windowslive.com>
2021-06-20 15:42:00 +0300

    Added the computerPlay fucntion to the JavaScript

--- scripts/rps.js
+++ scripts/rps.js
@@ -1,1 +1,28 @@
-console.log("Hello World!");
+/*console.log("Hello World!");
+
+The above code was an initial test */
+
+function computerPlay(){
+  let choice = 0;
+
+  //Give choice a random value between 0 and 2
+  choice = Math.floor(Math.random()*3);
+
+  //Switch statement that outputs either Rock, Paper or Scissors
+  switch(choice){
+    case 0:
+      console.log("Rock");
+      break;
+    case 1:
+      console.log("Paper");
+      break;
+    case 2:
+      console.log("Scissors");
+      break;
+    default:
+      console.log("Error");
+  }
+}
+
+//Testing computerPlay() function
+computerPlay();
